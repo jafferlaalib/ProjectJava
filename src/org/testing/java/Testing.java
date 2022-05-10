2cc3d8c43a9d6dd14035c0cfcf28bd850aed2f72
jafferlaalib <jafferlaalib1620@gmail.com>
2022-05-10 23:35:46 +0530

    bilaal edited

--- src/org/testing/java/Testing.java
+++ src/org/testing/java/Testing.java
@@ -1,5 +1,14 @@
 package org.testing.java;
 
 public class Testing {
+	public static void main(String[] args) {
+		
+		String a = "hello";
+		String b = "java";
+		
+		System.out.println(a+ "\t" + b);		
+		
+		
+	}
 
 }
